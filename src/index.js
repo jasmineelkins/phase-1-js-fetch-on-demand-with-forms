f3b44b49b808282ff39046dbc9c807cf8d39ccc4
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-03-02 13:15:59 -0500

    commit tree 4ef70e01ddb9eaad6de974e7a9bfc241104c779e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const init = () => {
-  
-}
+// Any code related to DOM manipulation should go in init or a fn called within init
 
-document.addEventListener('DOMContentLoaded', init);
+const init = () => {
+  const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.children[1];
+    // const input = document.querySelector("input#searchByID")
+
+    console.log(input.value);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // replace some HTML content with data retrieved from the server
+        // first access the content from HTML:
+
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
